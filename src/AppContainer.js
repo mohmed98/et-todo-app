fe9c7232139220fefbdb4329a6d2c8af72abfa48
mohamed98 <mohamedatta447@gmail.com>
2023-11-22 03:16:46 +0200

    Update UI for new state management (WIP)

---
+++ src/AppContainer.js
@@ -0,0 +1,18 @@
+import App from './App';
+import {Container} from 'flux/utils';
+import TodoStore from './data/TodoStore';
+import TodoActions from './data/TodoActions';
+
+function getStores() {
+  return [TodoStore];
+}
+
+function getState() {
+  return {
+    todos: TodoStore.getState(),
+    onDeleteTodo: TodoActions.deleteTodo,
+    onToggleTodo: TodoActions.toggleTodo,
+  };
+}
+
+export default Container.createFunctional(App, getStores, getState);
